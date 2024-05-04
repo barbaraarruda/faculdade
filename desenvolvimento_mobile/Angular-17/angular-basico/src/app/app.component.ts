6578ca78a599631451f8b3ab72ef0c2b5d06f612
barbaraarruda <barbarahmiley@gmail.com>
2024-05-04 12:20:31 -0300

    aula 06 event binding

--- desenvolvimento_mobile/Angular-17/angular-basico/src/app/app.component.ts
+++ desenvolvimento_mobile/Angular-17/angular-basico/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Componente01Component } from './componente01/componente01.component';
 import { CommonModule } from '@angular/common';
+import { Componente02Component } from "./componente02/componente02.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, Componente01Component],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component]
 })
 export class AppComponent {
   title = 'angular-basico';
